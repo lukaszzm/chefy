dddc1785f284d60e2dd71213b6628b9b838dfaa6
LukaszzM <lukaszzmalucha@gmail.com>
2023-02-24 16:07:29 +0100

    FIX: blue color on Tag

--- components/UI/Tag.tsx
+++ components/UI/Tag.tsx
@@ -12,7 +12,7 @@ export const Tag: React.FC<ITagProps> = (props) => {
   const style = classNames(
     "font-medium text-sm p-1 h-8 m-1 text-gray-700 bg-gray-100 border-2 border-gray-200 rounded-lg text-center",
     {
-      "bg-blue-500 text-white border-blue-500": color === "blue",
+      "bg-blue-600 text-white border-blue-600": color === "blue",
       "bg-orange-600 text-white border-orange-600": color === "orange",
     }
   );
