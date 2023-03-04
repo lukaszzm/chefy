c469a59fca20f1f3423ea517c02029a0e3fcdec0
Łukasz <lukasz.malucha1@gmail.com>
2023-03-04 14:19:12 +0100

    ADD: animation for home screen

--- components/Home/Navbar.tsx
+++ components/Home/Navbar.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import { Title } from "../UI/Title";
 import { Button } from "../UI/Button";
 import { Logo } from "../UI/Logo";
 
@@ -11,7 +9,7 @@ export const NavBar: React.FC<INavBarProps> = (props) => {
   const { openLoginModal } = props;
 
   return (
-    <div className="fixed flex flex-wrap justify-between items-center p-2 px-4 w-full font-medium text-xl border-b-2">
+    <div className="fixed h-22 flex bg-gray-100 z-20 flex-wrap justify-between items-center p-2 px-4 w-full font-medium text-xl border-b-2">
       <Logo />
       <div className="flex items-center">
         <Button type="primary" onClick={openLoginModal} className="px-6">
