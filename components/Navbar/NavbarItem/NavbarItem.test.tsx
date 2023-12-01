022565cd5a131808e339fe017c25851bdbfe1f6f
LukaszzM <lukaszzmalucha@gmail.com>
2023-12-01 19:34:55 +0100

    Change yarn to npm

--- components/Navbar/NavbarItem/NavbarItem.test.tsx
+++ components/Navbar/NavbarItem/NavbarItem.test.tsx
@@ -1,12 +1,17 @@
 import { render, screen, within } from "@testing-library/react";
 import { NavbarItem } from "./NavbarItem";
+import Image from "next/image";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
 describe("NavbarItem", () => {
   it("should render correctly as a button", () => {
     render(
-      <NavbarItem icon={<img alt="test icon" />} text="test" type="button" />
+      <NavbarItem
+        icon={<Image src="" alt="test icon" />}
+        text="test"
+        type="button"
+      />
     );
 
     const button = screen.getByRole("button", {
@@ -22,7 +27,7 @@ describe("NavbarItem", () => {
   });
 
   it("should render correctly as a link", () => {
-    render(<NavbarItem icon={<img alt="test icon" />} text="test" />);
+    render(<NavbarItem icon={<Image src="" alt="test icon" />} text="test" />);
 
     const link = screen.getByRole("link", {
       name: /test/i,
