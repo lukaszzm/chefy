a48be07fc4724d0aa51a9a78340ff1521878332e
LukaszzM <lukaszzmalucha@gmail.com>
2024-03-22 16:35:33 +0100

    fix: NavBarItem test

--- components/Navbar/NavbarItem/NavbarItem.test.tsx
+++ components/Navbar/NavbarItem/NavbarItem.test.tsx
@@ -1,14 +1,16 @@
-import { render, screen, within } from "@testing-library/react";
+import { getByTestId, render, screen, within } from "@testing-library/react";
 import { NavbarItem } from "./NavbarItem";
 import Image from "next/image";
 
 jest.mock("next/router", () => require("next-router-mock"));
 
+const testIcon = <span data-testid="test-icon">icon</span>
+
 describe("NavbarItem", () => {
   it("should render correctly as a button", () => {
     render(
       <NavbarItem
-        icon={<Image src="" alt="test icon" />}
+        icon={testIcon}
         text="test"
         type="button"
       />
@@ -17,25 +19,20 @@ describe("NavbarItem", () => {
     const button = screen.getByRole("button", {
       name: /test/i,
     });
-    const { getByRole } = within(button);
-    const icon = getByRole("img", {
-      name: /test icon/i,
-    });
+    const icon = within(button).getByTestId("test-icon");
 
     expect(button).toBeInTheDocument();
     expect(icon).toBeInTheDocument();
   });
 
   it("should render correctly as a link", () => {
-    render(<NavbarItem icon={<Image src="" alt="test icon" />} text="test" />);
+    render(<NavbarItem icon={testIcon} text="test" />);
 
     const link = screen.getByRole("link", {
       name: /test/i,
     });
-    const { getByRole } = within(link);
-    const icon = getByRole("img", {
-      name: /test icon/i,
-    });
+    const icon = within(link).getByTestId("test-icon");
+
 
     expect(link).toBeInTheDocument();
     expect(icon).toBeInTheDocument();
