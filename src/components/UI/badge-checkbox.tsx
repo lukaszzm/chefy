de00a3e7250233641baf1a5b6faecfe1c65ba082
LukaszzM <lukaszzmalucha@gmail.com>
2024-04-20 18:07:21 +0200

    feat: Add BadgeCheckbox

---
+++ src/components/UI/badge-checkbox.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import type { InputProps } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface BadgeCheckboxProps extends InputProps {
+  id: string;
+  label: string;
+}
+
+export const BadgeCheckbox = React.forwardRef<HTMLInputElement, BadgeCheckboxProps>(({ id, label, ...props }, ref) => {
+  return (
+    <>
+      <input className="peer hidden" id={id} ref={ref} type="checkbox" {...props} />
+      <Label
+        className="inline-flex cursor-pointer rounded-lg border border-border bg-muted/20 px-3.5 py-1.5 text-center text-xs text-muted-foreground transition duration-150 ease-in-out  hover:bg-muted/80 peer-checked:border-primary peer-checked:text-primary  peer-checked:hover:bg-accent"
+        htmlFor={id}
+      >
+        {label}
+      </Label>
+    </>
+  );
+});
+
+BadgeCheckbox.displayName = "BadgeCheckbox";
--- src/components/settings/preferences/preferences.tsx
+++ src/components/settings/preferences/preferences.tsx
@@ -1,3 +1,12 @@
+import { CategoriesForm } from "@/components/settings/preferences/categories-form";
+import { SettingsContainer } from "@/components/settings/settings-container";
+
 export const Preferences = () => {
-  return <div>Preferences</div>;
+  return (
+    <div className="space-y-3">
+      <SettingsContainer subtitle="Categories">
+        <CategoriesForm />
+      </SettingsContainer>
+    </div>
+  );
 };
