b6ee04a9f27a203fa1cc2fef2ca0d61ceece43e9
LukaszzM <lukaszzmalucha@gmail.com>
2024-04-22 19:14:58 +0200

    feat: Return recipes by user preferences

--- src/actions/recipe/get-suggested-recipes.ts
+++ src/actions/recipe/get-suggested-recipes.ts
@@ -1,10 +1,18 @@
 "use server";
 
-import { notInArray, and, eq } from "drizzle-orm";
+import { notInArray, and, eq, inArray } from "drizzle-orm";
 
 import { validateRequest } from "@/lib/auth";
 import db from "@/lib/db";
-import { area, category, recipe, userDislikedRecipe, userLikedRecipe } from "@/lib/db/schema";
+import {
+  area,
+  category,
+  recipe,
+  userDislikedRecipe,
+  userLikedRecipe,
+  userPreferredArea,
+  userPreferredCategory,
+} from "@/lib/db/schema";
 import type { SuggestedRecipe } from "@/types";
 
 export const getSuggestedRecipes = async (): Promise<Array<SuggestedRecipe>> => {
@@ -14,20 +22,19 @@ export const getSuggestedRecipes = async (): Promise<Array<SuggestedRecipe>> => 
     throw new Error("Unauthorized");
   }
 
-  // TODO: uncomment this when the user preferences are implemented
-  // const preferredAreas = db
-  //   .select({
-  //     areaId: userPreferredArea.areaId,
-  //   })
-  //   .from(userPreferredArea)
-  //   .where(eq(userPreferredArea.userId, user.id));
+  const preferredAreas = db
+    .select({
+      areaId: userPreferredArea.areaId,
+    })
+    .from(userPreferredArea)
+    .where(eq(userPreferredArea.userId, user.id));
 
-  // const preferredCategories = db
-  //   .select({
-  //     categoryId: userPreferredCategory.categoryId,
-  //   })
-  //   .from(userPreferredCategory)
-  //   .where(eq(userPreferredCategory.userId, user.id));
+  const preferredCategories = db
+    .select({
+      categoryId: userPreferredCategory.categoryId,
+    })
+    .from(userPreferredCategory)
+    .where(eq(userPreferredCategory.userId, user.id));
 
   const likedRecipes = db
     .select({
@@ -47,10 +54,12 @@ export const getSuggestedRecipes = async (): Promise<Array<SuggestedRecipe>> => 
     .select()
     .from(recipe)
     .where(
-      and(notInArray(recipe.id, likedRecipes), notInArray(recipe.id, dislikedRecipes))
-      // TODO: Implement this
-      // inArray(recipe.areaId, preferredAreas),
-      // inArray(recipe.categoryId, preferredCategories)
+      and(
+        notInArray(recipe.id, likedRecipes),
+        notInArray(recipe.id, dislikedRecipes),
+        inArray(recipe.areaId, preferredAreas),
+        inArray(recipe.categoryId, preferredCategories)
+      )
     )
     .innerJoin(category, eq(recipe.categoryId, category.id))
     .innerJoin(area, eq(recipe.areaId, area.id))
