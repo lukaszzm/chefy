8146255545f93bc91ba8c03acd3299eaf73e5125
LukaszzM <lukaszzmalucha@gmail.com>
2024-04-23 18:12:11 +0200

    refactor: Remove unused files

--- src/api-helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export { withAuth } from "./withAuth";
-export { withMethods } from "./withMethods";
-export { withValidation } from "./withValidation";
--- src/utils/isItemChosen.ts
+++
@@ -1,9 +0,0 @@
-import type { Category, Area } from "@/interfaces";
-
-export const isItemChosen = (el: Category | Area, list: Category[] | Area[]) => {
-  for (const category of list) {
-    if (category.id === el.id) return true;
-  }
-
-  return false;
-};
