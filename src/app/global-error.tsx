a6328060f6bd9c3cc20a798b7de9b05ff530069d
LukaszzM <lukaszzmalucha@gmail.com>
2024-04-27 14:55:48 +0200

    feat: Add global error handler

---
+++ src/app/global-error.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ServerCrash } from "lucide-react";
+
+import { Container } from "@/components/ui/container";
+
+export default function GlobalError() {
+  return (
+    <html>
+      <body className="flex min-h-screen items-center justify-center bg-popover text-center sm:bg-background">
+        <Container className="items-center">
+          <ServerCrash size={44} />
+          <h1 className="text-2xl font-semibold">Internal Server Error</h1>
+          <p className="text-muted-foreground">Sorry, something went wrong on our servers. Please try again later.</p>
+        </Container>
+      </body>
+    </html>
+  );
+}
