aa928b1d2d17c97ade77cb8ec21eac77c14eb022
LukaszzM <lukaszzmalucha@gmail.com>
2024-05-09 15:54:21 +0200

    fix: Get liked recipe from db

--- src/features/likes/actions/delete-like.ts
+++ src/features/likes/actions/delete-like.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 
 import { routes } from "@/config/routes";
 import { validateRequest } from "@/lib/auth";
-import { deleteLikeRecipe, getLikeRecipeById } from "@/lib/db/queries/recipe";
+import { deleteLikeRecipe, getLikeRecipe } from "@/lib/db/queries/recipe";
 import { errorResponse, successResponse } from "@/utils/action-response";
 
 export const deleteLike = async (recipeId: string, withRedirect: boolean) => {
@@ -15,7 +15,7 @@ export const deleteLike = async (recipeId: string, withRedirect: boolean) => {
     return errorResponse("Unauthorized");
   }
 
-  const like = await getLikeRecipeById(recipeId);
+  const like = await getLikeRecipe(recipeId, user.id);
 
   if (!like) {
     return errorResponse("Like not found");
